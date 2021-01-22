62afaf69a0766f6a13b8f0523ea6f3f35fe87841
bethanyrennard <bethany.rennard@gmail.com>
2021-01-21 20:04:08 -0800

    updated homepage

--- sketch.js
+++ sketch.js
@@ -24,27 +24,29 @@ background(0);
 image(img, windowWidth/2, windowHeight/2, 800, 800);
 
 if (mouseX < windowWidth/2 && mouseX > windowWidth/5){
-    s = map(mouseX, 0, windowHeight, 95, 20);
-    d = map(mouseX, 0, windowHeight, 20, 95);
+    s = map(mouseY, 0, windowHeight, 95, 20);
+    d = map(mouseX, 0, windowWidth, 20, 95);
+    // s = 45;
+    // d = 45;
 
-    r = map(mouseY, 0, windowHeight, 255, 0);
-    b = map(mouseX, windowWidth/5, windowWidth/2, 0, 255);
+    // r = map(mouseY, 0, windowHeight, 255, 0);
+    // b = map(mouseX, windowWidth/5, windowWidth/2, 0, 255);
 
-   fill(0, r, b);
+   fill(0, 0, 0);
    noStroke();
 
    ellipse(windowWidth/2 - 133, windowHeight/2 - 240, s, s);
-   ellipse(windowWidth/2 - 19, windowHeight/2 - 37, 45, 45);
+   ellipse(windowWidth/2 - 19, windowHeight/2 - 37, d, d);
    ellipse(windowWidth/2 + 62, windowHeight/2 - 255, s, s);
    ellipse(windowWidth/2 + 140, windowHeight/2 + 290, s, s);
 
-   fill(b, 0, r);
+   fill(0, 0, 0);
    noStroke();
 
-   ellipse(windowWidth/2 - 59, windowHeight/2 - 180, 45, 45);
+   ellipse(windowWidth/2 - 59, windowHeight/2 - 180, s, s);
    ellipse(windowWidth/2 + 80, windowHeight/2 - 102, s, s);
    ellipse(windowWidth/2 + 62, windowHeight/2 + 73, d, d);
-   ellipse(windowWidth/2 + 105, windowHeight/2 + 220, 45, 45);
+   ellipse(windowWidth/2 + 105, windowHeight/2 + 220, s, s);
    ellipse(windowWidth/2 + 200, windowHeight/2 - 235, d, d);
    ellipse(windowWidth/2 - 165, windowHeight/2 - 102, d, d);
 
@@ -54,23 +56,26 @@ if (mouseX < windowWidth/2 && mouseX > windowWidth/5){
    s = map(mouseX, windowWidth/2, windowWidth, 20, 95);
    d = map(mouseX, windowWidth/2, windowWidth, 95, 20);
 
-   r = map(mouseY, 0, windowHeight, 0, 255);
-   b = map(mouseX, windowWidth/2, (windowWidth - windowWidth/5), 255, 0);
+   // s = 45;
+   // d = 45;
 
-   fill(0, r, b);
+   // r = map(mouseY, 0, windowHeight, 0, 255);
+   // b = map(mouseX, windowWidth/2, (windowWidth - windowWidth/5), 255, 0);
+
+   fill(0, 0, 0);
    noStroke();
 
    ellipse(windowWidth/2 - 19, windowHeight/2 - 102, d, d);
    ellipse(windowWidth/2 + 184, windowHeight/2 + 60, s, s);
-   ellipse(windowWidth/2 - 59, windowHeight/2 - 308, 45, 45);
+   ellipse(windowWidth/2 - 59, windowHeight/2 - 308, d, d);
    ellipse(windowWidth/2 - 165, windowHeight/2 - 37, s, s);
    ellipse(windowWidth/2 + 155, windowHeight/2 - 235, d, d);
 
-   fill(b, 0, r);
+   fill(0, 0, 0);
    noStroke();
 
-   ellipse(windowWidth/2 - 59, windowHeight/2 - 308, 45, 45);
+   ellipse(windowWidth/2 - 59, windowHeight/2 - 308, s, s);
    ellipse(windowWidth/2 + 105, windowHeight/2 + 290, d, d);
-   ellipse(windowWidth/2 + 217, windowHeight/2 + 220, 45, 45);
+   ellipse(windowWidth/2 + 217, windowHeight/2 + 220, s, s);
  }
 }
